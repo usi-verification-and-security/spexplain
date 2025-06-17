1823527f2bee407aef6426fa0bf1a568789403e6
Tomáš Kolárik <tomaqa@gmail.com>
2025-06-17 17:11:25 +0200

    encoding the NN using aux. variables of the neurons

--- src/spexplain/bin/main.cpp
+++ src/spexplain/bin/main.cpp
@@ -179,7 +179,8 @@ std::optional<int> getOpts(int argc, char * argv[], spexplain::Framework::Config
 
         switch (c) {
             case 0: {
-                std::string_view optargStr{optarg};
+                std::string_view optargStr;
+                if (optarg) { optargStr = optarg; }
                 switch (selectedLongOpt) {
                     case outputTimesLongOpt:
                         config.setTimesFileName(optarg);
@@ -194,7 +195,7 @@ std::optional<int> getOpts(int argc, char * argv[], spexplain::Framework::Config
                             config.printIntervalExplanationsInBoundFormat();
                         }
                         break;
-                    case filterLongOpt:
+                    case filterLongOpt: {
                         std::optional<bool> optCorrectnessFilter{};
                         if (optargStr.starts_with("in")) {
                             optargStr.remove_prefix(2);
@@ -223,6 +224,8 @@ std::optional<int> getOpts(int argc, char * argv[], spexplain::Framework::Config
                         }
 
                         assert(false);
+                        break;
+                    }
                 }
                 break;
             }
