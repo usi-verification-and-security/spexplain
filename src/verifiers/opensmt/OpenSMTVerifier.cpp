f6b931c248026f1f7629523cb02a899b1626fdc1
Tomáš Kolárik <tomaqa@gmail.com>
2025-12-18 11:50:03 +0100

    refactored asserting/resetting model (ground and sample-based) and added dummy preprocessing

--- src/verifiers/opensmt/OpenSMTVerifier.cpp
+++ src/verifiers/opensmt/OpenSMTVerifier.cpp
@@ -26,7 +26,7 @@ public:
 
     std::size_t termSizeOf(PTRef const &) const;
 
-    void loadModel(spexplain::Network const &);
+    void assertSampleModel(spexplain::Network const &);
 
     void setUnsatCoreFilter(std::vector<NodeIndex> const &);
 
@@ -73,7 +73,7 @@ public:
 
     void resetSampleQuery();
     void resetSample();
-    void reset();
+    void resetSampleModel();
 
     UnsatCore getUnsatCore() const;
 
@@ -141,8 +141,8 @@ std::size_t OpenSMTVerifier::termSizeOf(PTRef const & term) const {
     return pimpl->termSizeOf(term);
 }
 
-void OpenSMTVerifier::loadModel(spexplain::Network const & network) {
-    pimpl->loadModel(network);
+void OpenSMTVerifier::assertSampleModel(spexplain::Network const & network) {
+    pimpl->assertSampleModel(network);
 }
 
 void OpenSMTVerifier::setUnsatCoreFilter(std::vector<NodeIndex> const & filter) {
@@ -211,9 +211,9 @@ void OpenSMTVerifier::resetSample() {
     UnsatCoreVerifier::resetSample();
 }
 
-void OpenSMTVerifier::reset() {
-    pimpl->reset();
-    UnsatCoreVerifier::reset();
+void OpenSMTVerifier::resetSampleModel() {
+    pimpl->resetSampleModel();
+    UnsatCoreVerifier::resetSampleModel();
 }
 
 UnsatCore OpenSMTVerifier::getUnsatCore() const {
@@ -290,7 +290,8 @@ std::size_t OpenSMTVerifier::OpenSMTImpl::termSizeOf(PTRef const & term) const {
     return totalSize;
 }
 
-void OpenSMTVerifier::OpenSMTImpl::loadModel(spexplain::Network const & network) {
+//+ move same common parts into assertGroundModel, but incremental assertions seem much slower
+void OpenSMTVerifier::OpenSMTImpl::assertSampleModel(spexplain::Network const & network) {
     // Store information about layer sizes
     layerSizes.clear();
     for (LayerIndex layer = 0u; layer < network.getNumLayers(); layer++) {
@@ -584,7 +585,8 @@ void OpenSMTVerifier::OpenSMTImpl::resetSample() {
     // resetSampleQuery() is called by Verifier
 }
 
-void OpenSMTVerifier::OpenSMTImpl::reset() {
+//+ move sth. common into resetGroundModel
+void OpenSMTVerifier::OpenSMTImpl::resetSampleModel() {
     logic = std::make_unique<ArithLogic>(opensmt::Logic_t::QF_LRA);
     solver = std::make_unique<MainSolver>(*logic, *config, "verifier");
     inputVars.clear();
